157b965084c4a4742be9a9e6befb0905e4023aaf
madison mirelez <madis123879@gmail.com>
2025-06-07 23:22:24 -0500

    chore: code functions to tkae user input, evaluate, and update the displays

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
+const equationDisplay = document.querySelector('.display #equation' )
+const resultDisplay = document.querySelector('.display #result')
+const equals = document.querySelector('.symbol#equals')
 
+
+//change to undefined for input
+let num1 = 1
+let num2 = 2
+let operator = '+'
 
 function add(a,b){
     return a+b;
@@ -18,3 +26,32 @@ function divide(a,b){
     }
     return a/b
 }
+
+function operate(num1, num2, operator){
+    switch (operator) {
+        case '+':
+            return add(num1, num2);
+        case '-':
+            return subtract(num1,num2);
+        case 'x':
+            return multiply(num1,num2)           
+        case '÷':
+            return divide(num1,num2);
+    }
+}
+
+function run_calculation(n1, n2, sign){
+    equationDisplay.textContent = '';
+    equationDisplay.textContent = `${n1}${sign}${n2}= `;
+    resultDisplay.textContent = '';
+    resultDisplay.textContent = `${operate(n1, n2, sign)}`;
+    num1 = operate(n1, n2, sign);
+    operator = undefined;
+    num2 = undefined;
+    }
+
+equals.addEventListener('click', () => {
+    if (num1 && num2 && operator){
+        run_calculation(num1, num2, operator);
+    }
+})
